5010c778c0168449afac2887538e47eeff60226f
Faris <85644267+9faris@users.noreply.github.com>
2021-07-21 12:47:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var open = document.getElementById('hamburger');
+var changeIcon = true;
+
+open.addEventListener("click", function(){
+
+    var overlay = document.querySelector('.overlay');
+    var nav = document.querySelector('nav');
+    var icon = document.querySelector('.menu-toggle i');
+
+    overlay.classList.toggle("menu-open");
+    nav.classList.toggle("menu-open");
+
+    if (changeIcon) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-times");
+
+        changeIcon = false;
+    }
+    else {
+        icon.classList.remove("fa-times");
+        icon.classList.add("fa-bars");
+        changeIcon = true;
+    }
+});
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      });
+  });
+
+  var MenuItems = document.getElementById("MenuItems");
+  MenuItems.style.maxHeight = "0px"
+
+  function menutoggle(){
+      if (MenuItems.style.maxHeight == "0px")
+      {
+          MenuItems.style.maxHeight = "200px";
+      }
+
+      else{
+          MenuItems.style.maxHeight = "0px";
+      }
+  }
